dc11bf1c7654175a73064e4087b8f3066c8cecee
Mike Pogue <mpogue@zenstarstudio.com>
2016-11-18 22:55:25 -0800

    Remember previous window placement
    When the main window is closed, remember the placement of the window, and restore it at next startup time.  Tested on Mac OS X Sierra and Win10.

--- SquareDesk-DEV/test123/main.cpp
+++ SquareDesk-DEV/test123/main.cpp
@@ -9,6 +9,13 @@ int main(int argc, char *argv[])
     a.setOrganizationDomain("zenstarstudio.com");
 
     MainWindow w;
+
+    // put window back where it was last time (modulo the screen size, which
+    //   is automatically taken care of.
+    QSettings settings;
+    w.restoreGeometry(settings.value("geometry").toByteArray());
+    w.restoreState(settings.value("windowState").toByteArray());
+
     w.show();
 
     // http://stackoverflow.com/questions/6516299/qt-c-icons-not-showing-up-when-program-is-run-under-windows-o-s
