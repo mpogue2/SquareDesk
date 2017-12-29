59fba5c795e2e285ad0c2c4077c8602598d479a4
Mike Pogue <mpogue@zenstarstudio.com>
2017-12-28 23:07:27 -0800

    Removed some debug output
    And removed some debug output on myDateTimeEdit that I accidentally left in.

--- SquareDesk-DEV/test123/mydatetimeedit.cpp
+++ SquareDesk-DEV/test123/mydatetimeedit.cpp
@@ -21,20 +21,20 @@ QDateTimeEdit::StepEnabled myDateTimeEdit::stepEnabled() const
 void myDateTimeEdit::stepBy(int steps)
 {
     QTime currentTime(QDateTimeEdit::time());
-    qDebug() << "currentTime: " << currentTime;
+//    qDebug() << "currentTime: " << currentTime;
 
     QDateTimeEdit::Section temp_section = currentSection();
     if (temp_section == QDateTimeEdit::MinuteSection) {
-        qDebug() << "current section is MINUTES";
+//        qDebug() << "current section is MINUTES";
     } else if (temp_section == QDateTimeEdit::SecondSection) {
-        qDebug() << "current section is SECONDS";
+//        qDebug() << "current section is SECONDS";
         QTime next_time(currentTime.addSecs(steps));
         if (next_time.minute() != currentTime.minute()) {
             QDateTimeEdit::setTime(next_time);
             steps = 0;
         }
     } else if (temp_section == QDateTimeEdit::MSecSection) {
-        qDebug() << "current section is MILLISECONDS";
+//        qDebug() << "current section is MILLISECONDS";
         QTime next_time(currentTime.addMSecs(steps));
         if (next_time.second() != currentTime.second() ||
                 next_time.minute() != currentTime.minute()) {
