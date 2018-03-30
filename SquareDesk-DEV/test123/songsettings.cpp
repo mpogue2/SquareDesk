ea4840f660b04231a398cdf428c2b93ce4b7aa3a
Dan Lyke <danlyke@flutterby.com>
2018-03-29 17:54:02 -0700

    Added a default set of tags if none are set, for #38

--- SquareDesk-DEV/test123/songsettings.cpp
+++ SquareDesk-DEV/test123/songsettings.cpp
@@ -505,6 +505,8 @@ void SongSettings::removeTags(const QString &str)
         if (tagCounts.contains(tag))
         {
             tagCounts[tag] = tagCounts[tag] - 1;
+            if (tagCounts[tag] == 0)
+                tagCounts.remove(tag);
         }
         else
         {
@@ -543,6 +545,27 @@ QHash<QString,QPair<QString,QString>> SongSettings::getTagColors(bool loadCache)
             }
         }
     }
+    // do this even if we aren't caching.
+    if (colors.empty())
+    {
+        colors["irish"] = QPair<QString,QString>("#44dd44", "#004400");
+        colors["xmas"]  = QPair<QString,QString>("#008800", "#ffaaaa");
+        colors["sfgs"] = QPair<QString,QString>("#99cde3","#000000");
+        colors["teach"] = QPair<QString,QString>("#d9a6e3","#000000");
+        colors["closer"] = QPair<QString,QString>("#0000ff","#ffffff");
+        colors["Latin"] = QPair<QString,QString>("#099f9d","#bebf23");
+        colors["HOT"] = QPair<QString,QString>("#ff0000","#000000");
+        colors["Swing"] = QPair<QString,QString>("#ffdd00","#0000ff");
+        colors["Bollywood"] = QPair<QString,QString>("#ff8b00","#000000");
+        colors["DUET"] = QPair<QString,QString>("#ae8eff","#781e19");
+
+        for (auto color = colors.cbegin(); color != colors.cend(); ++color)
+        {
+            tagCounts[color.key()] = 1;
+        }
+        setTagColors(colors);
+    }
+    
     if (loadCache)
     {
         tagColorCache = colors;
