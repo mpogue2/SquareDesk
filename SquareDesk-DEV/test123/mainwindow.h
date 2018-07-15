f334ceb933631027ca85b23139b5f751e4722b7d
Mike Pogue <mpogue@zenstarstudio.com>
2018-07-15 13:51:25 -0700

    Prep for pre-alpha release (Mac OS X only)
    0.9.2alpha7b

--- SquareDesk-DEV/test123/mainwindow.h
+++ SquareDesk-DEV/test123/mainwindow.h
@@ -162,7 +162,7 @@ public:
 // REMEMBER TO CHANGE THIS WHEN WE RELEASE A NEW VERSION.
 //  Also remember to change the "latest" file on GitHub!
 
-#define VERSIONSTRING "0.9.2alpha7"
+#define VERSIONSTRING "0.9.2alpha7b"
 
 // cuesheets are assumed to be at the top level of the SquareDesk repo, and they
 //   will be fetched from there.
