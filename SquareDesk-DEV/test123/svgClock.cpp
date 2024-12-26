c1b3a1c07df05b78464a09078c5a80a1e6424b1c
Mike Pogue <mpogue@zenstarstudio.com>
2024-12-25 16:44:33 -0800

    Added copyrights where they were missing

--- SquareDesk-DEV/test123/svgClock.cpp
+++ SquareDesk-DEV/test123/svgClock.cpp
@@ -1,3 +1,28 @@
+/****************************************************************************
+**
+** Copyright (C) 2016-2024 Mike Pogue, Dan Lyke
+** Contact: mpogue @ zenstarstudio.com
+**
+** This file is part of the SquareDesk application.
+**
+** $SQUAREDESK_BEGIN_LICENSE$
+**
+** Commercial License Usage
+** For commercial licensing terms and conditions, contact the authors via the
+** email address above.
+**
+** GNU General Public License Usage
+** This file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appear in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file.
+**
+** $SQUAREDESK_END_LICENSE$
+**
+****************************************************************************/
+
 #include <QFile>
 #include <QMenu>
 #include <QRegularExpression>
