2805b6ad2e8cf09a72da0b9b44ed8601f4660dbd
Mike Pogue <mpogue@zenstarstudio.com>
2025-05-26 20:23:31 -0700

    #1411: prep for 1.1.2 release
    Only 3 lines need to change.

--- SquareDesk-DEV/test123/mainwindow.h
+++ SquareDesk-DEV/test123/mainwindow.h
@@ -222,7 +222,7 @@ public:
 //   remember to change the version in the .plist file
 
 // Also remember to change the "latest" file on GitHub (for Beta releases)!
-#define VERSIONSTRING "1.1.1"
+#define VERSIONSTRING "1.1.2"
 
 // cuesheets are assumed to be at the top level of the SquareDesk repo, and they
 //   will be fetched from there.
