5a16f80c62c8a03ec0eb60bd1935a277b8531649
Mike Pogue <mpogue@zenstarstudio.com>
2025-07-06 22:18:57 -0700

    #1460: Prep for V1.1.4
    The usual 3 places.

--- SquareDesk-DEV/test123/mainwindow.h
+++ SquareDesk-DEV/test123/mainwindow.h
@@ -128,7 +128,7 @@ class CuesheetMatchingDebugDialog;
 //   remember to change the version in the .plist file
 
 // Also remember to change the "latest" file on GitHub (for Beta releases)!
-#define VERSIONSTRING "1.1.3"
+#define VERSIONSTRING "1.1.4"
 
 // cuesheets are assumed to be at the top level of the SquareDesk repo, and they
 //   will be fetched from there.
