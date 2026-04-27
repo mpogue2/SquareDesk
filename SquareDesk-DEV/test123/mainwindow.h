4f4ea4f572e42d65c59556bda02170dbbd735262
Mike Pogue <mpogue@zenstarstudio.com>
2026-04-26 22:12:30 -0700

    #1588: Font sizes must not change when drop operation happens

--- SquareDesk-DEV/test123/mainwindow.h
+++ SquareDesk-DEV/test123/mainwindow.h
@@ -438,6 +438,8 @@ public:
     static void customMessageOutputQt(QtMsgType type, const QMessageLogContext &context, const QString &msg);
     static QString logFilePath;
 
+    void adjustFontSizes();
+
 public slots:
     // ============================================================================
     // UI EVENT SLOTS
@@ -974,7 +976,6 @@ private:
     int pointSizeToIndex(int pointSize);
     int indexToPointSize(int index);
     void setFontSizes();
-    void adjustFontSizes();
     void usePersistentFontSize();
     void persistNewFontSize(int points);
     void zoomInOut(int increment);
