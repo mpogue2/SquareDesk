fe6740e66677cc78f00c5202557d2ac744a4c590
Mike Pogue <mpogue@zenstarstudio.com>
2026-07-30 22:57:45 -0700

    #1677: change default embedded server port number range
    to avoid getting polled every few seconds by LLM front ends.

--- SquareDesk-DEV/test123/embeddedserver.h
+++ SquareDesk-DEV/test123/embeddedserver.h
@@ -45,8 +45,12 @@ private:
     QString m_secretToken;
     QMimeDatabase m_mimeDb;
     
-    static constexpr quint16 MIN_PORT = 8000;
-    static constexpr quint16 MAX_PORT = 8999;
+    // NOTE: deliberately NOT the 8000-8999 range.  Ports like 8000/8080/8888 are heavily used by
+    //   other local dev servers and LLM front-ends.  If one of those apps isn't listening when we
+    //   start up, we grab "its" port, and then its client polls us with requests like /api/status,
+    //   which we log as bogus "File not found" warnings.  8930-8979 is a quiet, unregistered range.
+    static constexpr quint16 MIN_PORT = 8930;
+    static constexpr quint16 MAX_PORT = 8979;
 };
 
 #endif // EMBEDDEDSERVER_H
