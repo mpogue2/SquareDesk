e069c8185e875d78538cd4040b4fd7809dcd9377
Mike Pogue <mpogue@zenstarstudio.com>
2026-08-17 22:34:24 -0700

    #1708: ready for 1.1.14 release
    usual 3 places

--- SquareDesk-DEV/test123/mainwindow.h
+++ SquareDesk-DEV/test123/mainwindow.h
@@ -143,7 +143,7 @@ struct SongMatchInfo;
 //   remember to change the version in the .plist file
 
 // Also remember to change the "latest" file on GitHub (for Beta releases)!
-#define VERSIONSTRING "1.1.13"
+#define VERSIONSTRING "1.1.14"
 
 // cuesheets are assumed to be at the top level of the SquareDesk repo, and they
 //   will be fetched from there.
